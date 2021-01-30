cd6a1e3592e4b893c5e8a7bb4589a3f864c15063
Jeffrey Chan <jeffreychan34@gmail.com>
2021-01-30 12:04:29 -0800

    Resolve 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "mbed.h"
-
+// conf
 DigitalOut led(LED1);
 // This is a message from Spencer :)
 
 int main() {
 	// Demo code
+	// Jeffs message
 	while(true) {
 		led = !led;
-		wait(55);	
+		wait(0);
 	}	
 }
