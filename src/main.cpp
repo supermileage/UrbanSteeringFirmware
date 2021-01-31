9c3efca976315722333079399266b93e3de44db5
Spencer <hello@spencerperkins.me>
2021-01-31 00:17:03 -0800

    Fixing the test changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,12 @@
 #include "mbed.h"
-<<<<<<< HEAD
-// hello works
-// testing 12o3
-=======
-// conf
 
-DigitalOut led(LED1);
-// This is a message from Spencer :)
->>>>>>> main
+DigitalIn led(LED1);
 
-
-// this is very cool
 int main() {
-	// Demo code
-	// Jeffs message
 	while(true) {
 		led = !led;
-<<<<<<< HEAD
-		wait(22);
-=======
-		wait(52029340);	
->>>>>>> main
+		wait(5);	
 	}	
 }
 
 
-// Another message
