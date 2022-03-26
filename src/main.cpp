2f47ed98abf855e59db07cba4c105e9597247042
Colby <colby.r.sparks@gmail.com>
2022-03-25 20:15:20 -0700

    Adds can behavior on startup to trigger message send; adds g to global variables

--- src/main.cpp
+++ src/main.cpp
@@ -54,71 +54,58 @@ Timer timerDisplay;
 Timer timerAccessories;
 
 // globals variables shared between main and display threads
-unsigned _currentSpeed = 0;
-float _currentBmsSoc = 0.0f;
-float _currentBmsVoltage = 0.0f;
-uint8_t _currentThrottle = 0;
-int _lastGestureTime = 0;
-unsigned long _startTime = 0;
-char _ignitionVal = 0;
-char _dmsVal = 0;
-char _brakeVal = 0;
-char _lastGesture = 0;
+unsigned g_currentSpeed = 0;
+float g_currentBmsSoc = 0.0f;
+float g_currentBmsVoltage = 0.0f;
+uint8_t g_currentThrottle = 0;
+int g_lastGestureTime = 0;
+char g_prevAccVal = 0;
+char g_ignitionVal = 0;
+char g_dmsVal = 0;
+char g_brakeVal = 0;
+char g_lastGesture = 0;
 
 int main() {
     timerMotor.start();
     timerDisplay.start();
     timerAccessories.start();
-	char dummy;
-    uint8_t prev_dataStr = read_accessory_inputs(dummy);
-    uint8_t curr_dataStr = 0;
-    CANMessage msg;
-	_startTime = time(NULL);
+
+	// Initalize Accessories--it's impossible for left and right blinker to be on simultaneously so this will cause can update to be sent on boot
+    g_prevAccVal = 0xFF;
 
 	initialize_display();
 	Thread display_thread;
 	display_thread.start(display_task);
 
     while(1){
-		// check for time-reset gesture
-		listen_reset_gesture();
+		handle_reset_gesture();
 
-		// Handle motor controller
-		if (timerMotor.read_ms() > MOTOR_CONTROLLER_TRANSMIT_INTERVAL) {
-            handle_motor_inputs();
-            timerMotor.reset();
-        }
+		handle_accessories();
 
-		// Handle accessories
-        if (timerAccessories.read_ms() > ACCESSORIES_TRANSMIT_INTERVAL){
-			char hazardsOn;
-            curr_dataStr = read_accessory_inputs(hazardsOn);
-            if ((curr_dataStr != prev_dataStr)) {
-				// turn hazards off
-				if (hazardsOn) {
-					const unsigned char data[] = { 0x2, 0x4 << 1, 0x5 << 1 };
-					can.write(CANMessage(CAN_ACC_OPERATION, data, 3));
-					wait_us(1000);
-				}
-                const char data[] = {0, curr_dataStr};
-                can.write(CANMessage(CAN_ACC_OPERATION, data, 2));
-                prev_dataStr = curr_dataStr;
-            }
-            timerAccessories.reset();
-        }
+        handle_motor_inputs();
 
-		// Check for gps speed / bms data updates from telemetry
-		if (can.read(msg)) {
-			if (msg.id == CAN_TELEMETRY_GPS_DATA) {
-				_currentSpeed = (unsigned)msg.data[0];
-			} else if (msg.id == CAN_TELEMETRY_BMS_DATA) {
-				memcpy((void*)&_currentBmsSoc, (void*)msg.data, 4);
-				memcpy((void*)&_currentBmsVoltage, (void*)(msg.data + 4), 4);
-			}
-		}
+		receive_can();
     }
 }
 
+void handle_accessories() {
+	if (timerAccessories.read_ms() > ACCESSORIES_TRANSMIT_INTERVAL) {
+		char hazardsOn;
+		char currentAccVal = read_accessory_inputs(hazardsOn);
+		if ((currentAccVal != g_prevAccVal)) {
+			// turn hazards off
+			if (hazardsOn) {
+				const unsigned char data[] = { 0x2, 0x4 << 1, 0x5 << 1 };
+				can.write(CANMessage(CAN_ACC_OPERATION, data, 3));
+				wait_us(1000);
+			}
+			const char data[] = {0, currentAccVal};
+			can.write(CANMessage(CAN_ACC_OPERATION, data, 2));
+			g_prevAccVal = currentAccVal;
+		}
+		timerAccessories.reset();
+	}
+}
 
 char read_accessory_inputs(char& hazardsVal){
     char lightsVal = (char)(lights.read());
@@ -140,24 +127,29 @@ char read_accessory_inputs(char& hazardsVal){
 }
 
 void handle_motor_inputs(){
-    _dmsVal = get_dms_val();
-    _ignitionVal = (char)ignition.read();
-    _brakeVal = (char)!brake.read();
+	if (timerMotor.read_ms() > MOTOR_CONTROLLER_TRANSMIT_INTERVAL) {
 
-    _currentThrottle = get_throttle_val();
-    
-    if (!_dmsVal || !_ignitionVal || _brakeVal) {
-        _currentThrottle = 0;
-    }
+		g_dmsVal = get_dms_val();
+		g_ignitionVal = (char)ignition.read();
+		g_brakeVal = (char)!brake.read();
 
-	// Throttle Data
-	const unsigned char throttleData[] = { _currentThrottle };
-    can.write(CANMessage(CAN_STEERING_THROTTLE, throttleData, 1));
+		g_currentThrottle = get_throttle_val();
+		
+		if (!g_dmsVal || !g_ignitionVal || g_brakeVal) {
+			g_currentThrottle = 0;
+		}
 
-	// Ready Data
-    char readyVal = (_brakeVal << 2) | (_dmsVal << 1) | _ignitionVal;
-    const char readyData[] = { readyVal };
-    can.write(CANMessage(CAN_STEERING_READY, readyData, 1));
+		// Throttle Data
+		const unsigned char throttleData[] = { g_currentThrottle };
+		can.write(CANMessage(CAN_STEERING_THROTTLE, throttleData, 1));
+
+		// Ready Data
+		char readyVal = (g_brakeVal << 2) | (g_dmsVal << 1) | g_ignitionVal;
+		const char readyData[] = { readyVal };
+		can.write(CANMessage(CAN_STEERING_READY, readyData, 1));
+
+        timerMotor.reset();
+	}
 }
 
 unsigned char get_throttle_val() {
@@ -183,6 +175,20 @@ char get_dms_val() {
         dms_flag = 0;
 	}
     return dms_flag;
+}
+
+// Checks for gps speed / bms data updates from telemetry
+void receive_can() {
+    CANMessage msg;
+
+	if (can.read(msg)) {
+		if (msg.id == CAN_TELEMETRY_GPS_DATA) {
+			g_currentSpeed = (unsigned)msg.data[0];
+		} else if (msg.id == CAN_TELEMETRY_BMS_DATA) {
+			memcpy((void*)&g_currentBmsSoc, (void*)msg.data, 4);
+			memcpy((void*)&g_currentBmsVoltage, (void*)(msg.data + 4), 4);
+		}
+	}
 }
 
 
@@ -249,31 +255,31 @@ char get_dms_val() {
 
 
 // display data cache
-int _lastTime = 0;
-unsigned _lastSpeed = 0;
-uint8_t _lastThrottle = 0;
-float _lastBmsSoc = 0;
-float _lastBmsVoltage = 0;
-unsigned _lastMinutes = 0;
-unsigned _lastSeconds = 0;
-char _lastDmsVal = 0;
-char _lastIgnitionVal = 0;
-char _lastBrakeVal = 0;
+int g_lastTime = 0;
+unsigned g_lastSpeed = 0;
+uint8_t g_lastThrottle = 0;
+float g_lastBmsSoc = 0;
+float g_lastBmsVoltage = 0;
+unsigned g_lastMinutes = 0;
+unsigned g_lastSeconds = 0;
+char g_lastDmsVal = 0;
+char g_lastIgnitionVal = 0;
+char g_lastBrakeVal = 0;
 
-void listen_reset_gesture() {
+void handle_reset_gesture() {
 	char thisGesture = ignition.read();
 	int thisGestureTime = timerDisplay.read_ms();
-	if (_lastGestureTime + DEBOUNCE_TIME < thisGestureTime && _lastGesture != thisGesture) {
+	if (g_lastGestureTime + DEBOUNCE_TIME < thisGestureTime && g_lastGesture != thisGesture) {
 
-		if (thisGestureTime <= _lastGestureTime + GESTURE_MARGIN) {
-			_lastTime = -1;
-			_lastGestureTime = 0;
+		if (thisGestureTime <= g_lastGestureTime + GESTURE_MARGIN) {
+			g_lastTime = -1;
+			g_lastGestureTime = 0;
 			timerDisplay.reset();
 		} else {
-			_lastGestureTime = thisGestureTime;
+			g_lastGestureTime = thisGestureTime;
 		}
 	}
-	_lastGesture = thisGesture;
+	g_lastGesture = thisGesture;
 }
 
 void display_task() {
@@ -281,46 +287,46 @@ void display_task() {
 		TFT.set_font((unsigned char*) SMALL_FONT);
 
 		// Update DMS
-		if (_lastDmsVal != _dmsVal) {
-			if (_dmsVal) {
+		if (g_lastDmsVal != g_dmsVal) {
+			if (g_dmsVal) {
     			TFT.fillcircle(DMS_X + CIRCLE_X_OFFSET_DMS, CIRCLE_Y_OFFSET, CIRCLE_RADIUS, Green);
 			} else {
     			TFT.fillcircle(DMS_X + CIRCLE_X_OFFSET_DMS, CIRCLE_Y_OFFSET, CIRCLE_RADIUS, Red);
 			}
-			_lastDmsVal = _dmsVal;
+			g_lastDmsVal = g_dmsVal;
 		}
 
 		// Update Ignition
-		if (_lastIgnitionVal != _ignitionVal) {
-			if (_ignitionVal) {
+		if (g_lastIgnitionVal != g_ignitionVal) {
+			if (g_ignitionVal) {
 				TFT.fillcircle(IGNITION_X + CIRCLE_X_OFFSET_IGNITION, CIRCLE_Y_OFFSET, CIRCLE_RADIUS, Green);
 			} else {
 				TFT.fillcircle(IGNITION_X + CIRCLE_X_OFFSET_IGNITION, CIRCLE_Y_OFFSET, CIRCLE_RADIUS, Red);
 			}
-			_lastIgnitionVal = _ignitionVal;
+			g_lastIgnitionVal = g_ignitionVal;
 		}
 
 		// Update Brake
-		if (_lastBrakeVal != _brakeVal) {
-			if (!_brakeVal) {
+		if (g_lastBrakeVal != g_brakeVal) {
+			if (!g_brakeVal) {
 				TFT.fillcircle(BRAKE_X + CIRCLE_X_OFFSET_BRAKE, CIRCLE_Y_OFFSET, CIRCLE_RADIUS, Green);
 			} else {
 				TFT.fillcircle(BRAKE_X + CIRCLE_X_OFFSET_BRAKE, CIRCLE_Y_OFFSET, CIRCLE_RADIUS, Red);
 			}
-			_lastBrakeVal = _brakeVal;
+			g_lastBrakeVal = g_brakeVal;
 		}
 
 		// Update voltage
-		if (_lastBmsVoltage != _currentBmsVoltage) {
+		if (g_lastBmsVoltage != g_currentBmsVoltage) {
 			// update text
-			const char* padding = _currentBmsVoltage < 10.0f ? "0" : "";
+			const char* padding = g_currentBmsVoltage < 10.0f ? "0" : "";
 			TFT.locate(VOLTAGE_TEXT_X, VOLTAGE_TEXT_Y);
-			TFT.printf("%s%.2f", padding, _currentBmsVoltage);
-			_lastBmsVoltage = _currentBmsVoltage;
+			TFT.printf("%s%.2f", padding, g_currentBmsVoltage);
+			g_lastBmsVoltage = g_currentBmsVoltage;
 
 			// redraw rectangle
-			// float voltage = _currentBmsVoltage > 0.0f ? _currentBmsVoltage : -_currentBmsVoltage; // might not be necessary, unless negative voltage is possible?
-			int rectangleWidth = (int)(_currentBmsSoc / 100.0f * (float)VOLTAGE_WIDTH);
+			// float voltage = g_currentBmsVoltage > 0.0f ? g_currentBmsVoltage : -_currentBmsVoltage; // might not be necessary, unless negative voltage is possible?
+			int rectangleWidth = (int)(g_currentBmsSoc / 100.0f * (float)VOLTAGE_WIDTH);
 			TFT.fillrect(VOLTAGE_LEFT_X + VOLTAGE_PADDING, VOLTAGE_LEFT_Y + VOLTAGE_PADDING, VOLTAGE_LEFT_X - VOLTAGE_PADDING + rectangleWidth , VOLTAGE_RIGHT_Y - VOLTAGE_PADDING, Green);
 			TFT.fillrect(VOLTAGE_LEFT_X + VOLTAGE_PADDING + rectangleWidth, VOLTAGE_LEFT_Y + VOLTAGE_PADDING, VOLTAGE_RIGHT_X - VOLTAGE_PADDING, VOLTAGE_RIGHT_Y - VOLTAGE_PADDING, Black);
 		}
@@ -329,39 +335,39 @@ void display_task() {
 		TFT.set_font((unsigned char*) COOL_FONT);
 
 		// Update Speed
-		if (_lastSpeed != _currentSpeed) {
-			_lastSpeed = _currentSpeed;
+		if (g_lastSpeed != g_currentSpeed) {
+			g_lastSpeed = g_currentSpeed;
 			TFT.locate(SPEED_X, SPEED_Y);
-			TFT.printf("%02u", _currentSpeed);
+			TFT.printf("%02u", g_currentSpeed);
 		}
 
 		// Update Power
-		if(_lastThrottle != _currentThrottle) {
-			_lastThrottle = _currentThrottle;
+		if(g_lastThrottle != g_currentThrottle) {
+			g_lastThrottle = g_currentThrottle;
 			TFT.locate(POWER_X, POWER_Y);
-			TFT.printf("%03u", (_currentThrottle * 100) / 255);
+			TFT.printf("%03u", (g_currentThrottle * 100) / 255);
 			
 		}
 
 		// Update Time
 		int currentTime = timerDisplay.read_ms() / 1000;
-		if (currentTime != _lastTime) {
+		if (currentTime != g_lastTime) {
 			int minutes = currentTime / 60;
 			int seconds = currentTime % 60;
 
-			if(minutes != _lastMinutes){
+			if(minutes != g_lastMinutes){
 				TFT.locate(MINUTES_X, TIME_Y);
 				TFT.printf("%02u", minutes);
-				_lastMinutes = minutes;
+				g_lastMinutes = minutes;
 			}
 
-			if(seconds != _lastSeconds){
+			if(seconds != g_lastSeconds){
 				TFT.locate(SECONDS_X, TIME_Y);
 				TFT.printf("%02u", seconds);
-				_lastSeconds = seconds;
+				g_lastSeconds = seconds;
 			}
 
-			_lastTime = currentTime;
+			g_lastTime = currentTime;
 		}
 
 		// Set font size for large test display
